27f900f8d652b1c2bf3541d5a83b37ad2aaec0cf
juanjo44 <juanjosebetancourtosorio@gmail.com>
2022-03-15 10:44:45 -0500

    Go-First Project, art with Ascii

---
+++ Go/Project1/gopherIt.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("  __      _")
+	fmt.Println("o'')}____//")
+	fmt.Println(" `_/      )")
+	fmt.Println(" (_(_/-(_/")
+	fmt.Println(time.Now())
+}
