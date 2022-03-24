e7fb0353b7532b933ae20f3b7c5910ee9182c602
juanjo44 <juanjosebetancourtosorio@gmail.com>
2022-03-24 16:00:50 -0500

    HTML-fashionBlogs

---
+++ Go/MyOwnProjects/double_linked_list.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+//NODE STRUCTURE 
+type Node struct {
+	data int
+	right *Node
+	left *Node
+}
+
+type list struct {
+	len int
+	tail *Node
+	head *Node
+}
+func push(list *list, data int){
+	
+}
+func main(){
+
+}
