3a66fe1e4d97fe7c738a0cc75c9c0f80e028ef76
juanjo44 <juanjosebetancourtosorio@gmail.com>
2022-05-02 18:45:35 -0500

    Project number guesser ended

--- JavaScript/number-guesser-starting/script.js
+++ JavaScript/number-guesser-starting/script.js
@@ -7,9 +7,27 @@ let currentRoundNumber = 1;
 const generateTarget = () => Math.round(Math.random() *9);
 
 const compareGuesses = (human, computer, secret) => {
+    if(human < 0 || human > 9){
+        window.alert('You should choose a number between 0 and 9');
+    }
     if (Math.abs(secret - human) <= Math.abs(secret - computer)){
         return true;
     } else {
         return false;
     }
+}
+
+const updateScore = (value) =>{
+    switch (value){
+        case 'human':
+            humanScore += 1;
+            break;
+        default:
+            computerScore += 1;
+            break;
+
+    }
+}
+const advanceRound = () =>{
+    currentRoundNumber += 1;
 }
