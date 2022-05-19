cb9f29d51a8a93ac058adc038ea0b1643f829958
juanjo44 <juanjosebetancourtosorio@gmail.com>
2022-05-19 08:14:57 -0500

    Project dna js already ended

--- JavaScript/mystery-organism-starter/main.js
+++ JavaScript/mystery-organism-starter/main.js
@@ -15,7 +15,44 @@ const mockUpStrand = () => {
 
 
 
+const pAequorFactory = (number, dna) => {
+  return {
+    _specimenNum: number,
+    _dna: dna,
+    mutate () {
+      const randomPosition = [Math.floor(Math.random() * 15)]
+      const letter = this._dna[randomPosition];
 
+      do {
+        this._dna[randomPosition] = returnRandBase();
+      } while(this._dna[randomPosition] === letter)
+      return this._dna;
+    },
+
+    compareDNA (pAequor) {
+      let sameBase = 0;
+      for (let i = 0; i < 15; i++) {
+        if (pAequor._dna[i] === this._dna[i]){
+          sameBase++;
+        }
+      }
+      console.log(`specimen #1 and specimen #2 have ${(sameBase/15) * 100}% DNA in common`);
+    },
+
+    willLikelySurvive () {
+      let total = 0;
+      this._dna.forEach(ele => {
+        if (ele === 'C' || ele === 'G'){
+          total++;
+        }
+      })
+
+      return total >= 9;
+    }
+  };
+}
+
+console.log(pAequorFactory(1, mockUpStrand()));
 
 
 
